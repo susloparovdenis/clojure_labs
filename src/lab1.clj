ae2f930f3940fa5417e493bba6dfd6b236258ac6
Denis Susloparov <densus@gmail.com>
2021-09-16 03:50:13 +0700

    1.3

--- src/lab1.clj
+++ src/lab1.clj
@@ -16,3 +16,9 @@
                   head (remove (into #{} el) alph)]
               (conj el head))))))
 
+;1.3
+(defn my-map [f coll]
+  (reduce #(conj %1 (f %2)) [] coll))
+
+(defn  my-filter [f coll]
+  (reduce #(if (f %2) (conj %1 %2) %1) [] coll))
--- test/lab1_test.clj
+++ test/lab1_test.clj
@@ -7,6 +7,10 @@
     (is (= (perm [1 2 3] 2) '((1 2) (1 3) (2 1) (2 3) (3 1) (3 2))))))
 
 (deftest perm_tail-test
-  (testing "FIXME, I fail."
-    (is (= (perm_tail [1 2 3] 2) '((1 2) (1 3) (2 1) (2 3) (3 1) (3 2))))))
+    (is (= (perm_tail [1 2 3] 2) '((1 2) (1 3) (2 1) (2 3) (3 1) (3 2)))))
 
+(deftest my-map-test
+  (is (= (my-map inc [1 2 3 4] ) [2 3 4 5])))
+
+(deftest my-filter-test
+  (is (= (my-filter #(> %1 2) [1,2,3,4]) [3 4])))
