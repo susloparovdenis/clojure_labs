1575a15dc77b14f04d184638da7571e6b4a0894f
Denis Susloparov <densus@gmail.com>
2021-10-12 03:59:43 +0700

    cljfmt

--- src/lab1/lab1_3.clj
+++ src/lab1/lab1_3.clj
@@ -8,15 +8,14 @@
   [f coll]
   (reduce (fn [acc val] (concat acc (f val))) [] coll))
 
-
 (defn my-filter
   [f coll]
   (reduce
-    (fn [acc val]
-      (if (f val)
-        (conj acc val)
-        acc))
-    [] coll))
+   (fn [acc val]
+     (if (f val)
+       (conj acc val)
+       acc))
+   [] coll))
 
 (println (my-map (partial * 2) [1 2 3 4 5]))
 (println (my-filter (partial < 3) [1 2 3 4 5]))
